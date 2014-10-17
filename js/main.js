449946de417a12c1214d1c23e44b17e984bebe04
João Paulo Saragossa <joaosaragossa.dev@gmail.com>
2014-10-17 18:31:25 -0300

    workout

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 window.onload = function()
 {
   // Header height resize
-  
 
-  setInterval(function()
-  {
-    var header = document.getElementById('page-header').style.height = window.innerHeight + "px";
-  },0);
+  var header = document.getElementById('page-header');
+  
+  if (header){
+    setInterval(function()
+    {
+      header.style.height = window.innerHeight + "px";
+    },0);
+
+  };
+
+  // Post header height resize
+
+  var postHeader = document.getElementById('post-header');
+  
+  if (postHeader){
+    setInterval(function()
+    {
+      postHeader.style.height = window.innerHeight + "px";
+    },0);
+
+  };
 
   // Post list items height resize
 
   var posts = document.getElementById('post-list').getElementsByTagName('li');
 
-  setInterval(function()
-  {
-    for (var i = 0; i < posts.length; i++) 
+  if (posts){
+    setInterval(function()
     {
-      posts[i].style.height = posts[i].clientWidth + "px";
-    }
-  },0);
+      for (var i = 0; i < posts.length; i++) 
+      {
+        posts[i].style.height = posts[i].clientWidth + "px";
+      }
+    },0);
+  };
 }
